09c49f980675107b0a3d024f5c0730269323e6f2
TsungTai <z6031223@gmail.com>
2022-10-28 09:34:18 +0800

    AspNet HtmlHelper 練習

---
+++ WebApplication1/Helper/MyHtmlHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Helper
+{
+    public static class MyHtmlHelper
+    {
+        public static MvcHtmlString Hyperlink(this HtmlHelper helper, string url, string linkText)
+        {
+            return MvcHtmlString.Create(String.Format("<a href='{0}'>{1}</a>", url, linkText));
+        }
+    }
+}
